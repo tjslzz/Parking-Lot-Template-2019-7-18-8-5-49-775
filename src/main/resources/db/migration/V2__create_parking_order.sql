6701b5211766b1868e0eb607d8f96337ab5ad3df
JarryLi <3244188372@qq.com>
2019-07-18 20:03:27 +0800

    [jerryLi] build : add parking-order by flyway completely！

---
+++ src/main/resources/db/migration/V2__create_parking_order.sql
@@ -0,0 +1,10 @@
+CREATE TABLE parking_order  (
+  id int(255) UNSIGNED NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  number varchar(255) NOT NULL,
+  create_time varchar(255) NOT NULL,
+  end_time varchar(255) NOT NULL,
+  state int(1) UNSIGNED NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (`name`) REFERENCES `parking_lot` (`name`)
+)
